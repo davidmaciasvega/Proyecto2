730046814e1eb7939073e93148d3c5c08b529f8c
David <david.macias1354@alumnos.udg.mx>
2025-03-11 22:35:42 -0600

    3.5. El movimiento del jugador
    - El personaje debe poder desplazarse con las flechas de dirección.
    - Se respetan las físicas de colisiones programadas.

--- Proyecto2/Assets/Scripts/Goomba.cs
+++ Proyecto2/Assets/Scripts/Goomba.cs
@@ -15,24 +15,26 @@ public class Goomba : MonoBehaviour
         if (Profesor == null) return;
 
         Vector3 direction = Profesor.position - transform.position;
- if (direction.x >= 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f); // Mira a la derecha
-else transform.localScale = new Vector3(1.0f, 1.0f, 1.0f); 
+        if (direction.x >= 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f); // Mira a la derecha
+        else transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 
         float distance = Mathf.Abs(Profesor.position.x - transform.position.x);
 
         if (distance < 1.0f && Time.time > LastShoot + 0.25f)
         {
-            Shoot();
+            // Shoot(); // Comentado para evitar el error
             LastShoot = Time.time;
         }
     }
 
+    /*
     private void Shoot()
     {
         Vector3 direction = new Vector3(transform.localScale.x, 0.0f, 0.0f);
         GameObject bullet = Instantiate(BulletPrefab, transform.position + direction * 0.1f, Quaternion.identity);
         bullet.GetComponent<BulletScript>().SetDirection(direction);
     }
+    */
 
     public void Hit()
     {
