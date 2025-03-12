532d73c7db9f4b16392c3ee45ead5fa1b811ab08
David <david.macias1354@alumnos.udg.mx>
2025-03-12 16:23:23 -0600

    3.7. Efectos de acción
    En esta versión se implementa lo siguiente:
    - La bala programada no debe afectar la gravedad.
    - La trayectoria de la bala debe ser lineal.
    - La cantidad de balas que salen del arma debe ser uniforme, sin que se superpongan.

--- Proyecto2/Assets/Scripts/Score.cs
+++ Proyecto2/Assets/Scripts/Score.cs
@@ -1,25 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
     private int score;
+    public TextMeshProUGUI scoreText; 
+    public TextMeshProUGUI NivelText; 
 
     void Start()
     {
         score = 0;
         Debug.Log("Score está activo");
+        NivelText.text = "Nivel: " + (SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Moneda")
         {
-            score++; // Incrementar el score
+            score++;
             Debug.Log("¡Moneda recogida! Cantidad: " + score);
 
-            Destroy(collision.gameObject); // Destruir la moneda, no el Player
+            scoreText.text = "Score: " + score;
+            Destroy(collision.gameObject);
         }
     }
+
+    void Update()
+    {
+        NivelText.text = "Nivel: " + (SceneManager.GetActiveScene().buildIndex + 1);
+    }
 }
+
